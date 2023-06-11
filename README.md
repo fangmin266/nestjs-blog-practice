c07f2d4ad2059f973c3518460a338bc490267e46
fangmin26 <fangmin_@naver.com>
2023-06-11 20:16:41 +0900

    link,image,ssg,ssr,pre-rendering

---
+++ README.md
@@ -0,0 +1,41 @@
+### blog 프로젝트
+
+```
+yarn create next-app blog --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
+```
+
+1. Link 최적화
+
+- a 태그만 사용할 때보다 효용도가 높음 (전체 로드 x 필요한 js파일만 로드)
+- 뷰포트 내에서 노출될 때 로드가 가능(build하여 확인가능)
+- 클라이언트 링크로 연결할때만 사용
+
+```
+<Link href="/"><a>이동하기</a></Link>
+```
+
+2. layout/robots.txt/images
+
+```
+import Image from 'next/image'
+<img src="images/react.png" alt="react"/>
+<Image src="/images/react.png" width={200} height={200} react="react"/>
+//파일명 webp : 가벼운용량, 레이지로드(Link와 동일한 기능)
+```
+
+<img width="1501" alt="스크린샷 2023-06-11 오후 5 21 07" src="https://github.com/fangmin266/gitlevelup/assets/123913446/56d447a2-7739-4024-b4fa-cb4636a67c49">
+
+nestjs에서 제공하는 head 컴퍼너트에 title, image, description등 og tag , icon, third party script 추가 가능
+
+3. SSG(build time) / SSR(request time)
+   정적페이지
+   getStaticProps - SSG (marketing, blog, product list, help documnentation)
+   동적페이지
+   getServerSideProps - SSR
+   ISR, CSR
+
+4. pre-rendering, data-fetching
+
+```
+yarn add gray-matter (md 파일 파싱)
+```
---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import Image from 'next/image'
+
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/Layout'
+import utilStyles from '../styles/utils.module.css'
+// import { getSortedPostsData } from '../lib/post'
+import { useEffect, useState } from 'react'
+
+// export async function getStaticProps() { //SSG
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   }
+// }
+
+// export async function getServerSideProps() { //SSR
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   }
+// }
+
+export default function Home() {
+  // export default function Home({ allPostsData }) {
+  const [allPostsData, setAllpostData] = useState([]) // CSR
+  useEffect(() => {
+    fetch('/api/posts')
+      .then((res) => res.json())
+      .then((data) => setAllpostData(data.allPostsData))
+  }, [])
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
